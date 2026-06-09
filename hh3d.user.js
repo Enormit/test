a4510dc7d3f805e8be91787d28ed8a28b993c7e0
phamquyet472040 <3123411252@sv.sgu.edu.vn>
2026-06-09 23:02:46 +0700

    chore: v2.14 - update updateURL and downloadURL to new Enormit repository

--- hh3d.user.js
+++ hh3d.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name          HH3D Auto - v2.13
+// @name          HH3D Auto - v2.14
 // @namespace     hh3d-tool
-// @version       v2.13
-// @updateURL     https://raw.githubusercontent.com/phamquyet47204/tool-automation/main/hh3d.user.js
-// @downloadURL   https://raw.githubusercontent.com/phamquyet47204/tool-automation/main/hh3d.user.js
+// @version       v2.14
+// @updateURL     https://raw.githubusercontent.com/Enormit/tool-automation/main/hh3d.user.js
+// @downloadURL   https://raw.githubusercontent.com/Enormit/tool-automation/main/hh3d.user.js
 // @description   Auto  HH3D
-// @author        Cre: [Unknown] - v2.13
+// @author        Cre: [Unknown] - v2.14
 // @include       *://hoathinh3d.co*/*
 // @exclude       *://hoathinh3d.co/khoang-mach*
 // @require       https://cdn.jsdelivr.net/npm/sweetalert2@11.26.12/dist/sweetalert2.all.min.js
